c2aaab9f647831430b13977b1b88c1eed622f4df
Laishram Bandana Devi <laishrambandanadevi@Laishrams-MacBook-Pro.local>
2023-05-16 14:09:44 +0530

    revert: Adding placeholder image to improve LCP

--- libs/blocks/change-bg/change-bg.js
+++ libs/blocks/change-bg/change-bg.js
@@ -1,15 +1,10 @@
-import { getConfig, createTag } from '../../utils/utils.js';
+import { getConfig } from '../../utils/utils.js';
 
 const config = getConfig();
 const base = config.miloLibs || config.codeRoot;
-import(`${base}/deps/blades/blade-changebg.js`);
+await import(`${base}/deps/blades/blade-changebg.js`);
 
-function renderAsset(container, customElem, placeHolderImg) {
-  container.innerHTML = '';
-  container.append(placeHolderImg);
-  placeHolderImg.addEventListener('load', async () => {
-    await import(`${base}/deps/blades/blade-changebg.js`);
-  });
+function renderAsset(container, customElem) {
   container.innerHTML = '';
   container.append(customElem);
 }
@@ -17,6 +12,7 @@ function renderAsset(container, customElem, placeHolderImg) {
 export default function init(el) {
   const customElem = document.createElement('blade-changebg');
   customElem.classList.add('blade');
+
   // Extracting asset keys and urls and passing it to the custom element
   const images = el.querySelectorAll(':scope > div');
   images.forEach((item) => {
@@ -27,14 +23,13 @@ export default function init(el) {
     customElem.setAttribute(keyName, src);
   });
 
-  const placeHolderImg = createTag('img', { src: images[1]?.children[1]?.querySelectorAll(':scope > picture > img')[0]?.src, class: 'blade' });
   // Render it inside marquee if its previous sibling is a marquee block
   if (el.previousElementSibling?.classList.contains('marquee')) {
     const marquee = el.previousElementSibling;
     const container = marquee.querySelectorAll(':scope > div:last-child > div:last-child')[0];
-    renderAsset(container, customElem, placeHolderImg);
+    renderAsset(container, customElem);
     el.remove();
   } else {
-    renderAsset(el, customElem, placeHolderImg);
+    renderAsset(el, customElem);
   }
 }
