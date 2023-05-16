4746115f9ca287c551511bed4fc6f9cf7afeefbc
Laishram Bandana Devi <laishrambandanadevi@Laishrams-MacBook-Pro.local>
2023-05-16 10:49:57 +0530

    Adding placeholder image to improve LCP

--- libs/blocks/change-bg/change-bg.js
+++ libs/blocks/change-bg/change-bg.js
@@ -1,11 +1,20 @@
-import { getConfig } from '../../utils/utils.js';
+import { getConfig, createTag } from '../../utils/utils.js';
 
 const config = getConfig();
 const base = config.miloLibs || config.codeRoot;
-
 import(`${base}/deps/blades/blade-changebg.js`);
 
-export default async function init(el) {
+function renderAsset(container, customElem, placeHolderImg) {
+  container.innerHTML = '';
+  container.append(placeHolderImg);
+  placeHolderImg.addEventListener('load', async () => {
+    await import(`${base}/deps/blades/blade-changebg.js`);
+  });
+  container.innerHTML = '';
+  container.append(customElem);
+}
+
+export default function init(el) {
   const customElem = document.createElement('blade-changebg');
   customElem.classList.add('blade');
   // Extracting asset keys and urls and passing it to the custom element
@@ -17,15 +26,15 @@ export default async function init(el) {
     const src = content.querySelectorAll(':scope > picture > img')[0]?.src || content.querySelectorAll(':scope > a')[0]?.href;
     customElem.setAttribute(keyName, src);
   });
-  el.innerHTML = '';
-  el.append(customElem);
 
+  const placeHolderImg = createTag('img', { src: images[1]?.children[1]?.querySelectorAll(':scope > picture > img')[0]?.src, class: 'blade' });
   // Render it inside marquee if its previous sibling is a marquee block
   if (el.previousElementSibling?.classList.contains('marquee')) {
     const marquee = el.previousElementSibling;
     const container = marquee.querySelectorAll(':scope > div:last-child > div:last-child')[0];
-    container.innerHTML = '';
-    container.append(customElem);
+    renderAsset(container, customElem, placeHolderImg);
     el.remove();
+  } else {
+    renderAsset(el, customElem, placeHolderImg);
   }
 }
