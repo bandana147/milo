eb06d09c30a8b28aeaafd187cbe133247b360360
Laishram Bandana Devi <laishrambandanadevi@Laishrams-MacBook-Pro.local>
2023-05-16 13:12:05 +0530

    Adding placeholder image to improve LCP

--- libs/blocks/change-bg/change-bg.js
+++ libs/blocks/change-bg/change-bg.js
@@ -1,13 +1,22 @@
-import { getConfig } from '../../utils/utils.js';
+import { getConfig, createTag } from '../../utils/utils.js';
 
 const config = getConfig();
 const base = config.miloLibs || config.codeRoot;
 
-import(`${base}/deps/blades/blade-changebg.js`);
+function renderAsset(container, customElem, placeHolderImg) {
+  container.innerHTML = '';
+  container.append(placeHolderImg);
+  placeHolderImg.addEventListener('load', async () => {
+    await import(`${base}/deps/blades/blade-changebg.js`);
+  });
+  container.innerHTML = '';
+  container.append(customElem);
+}
 
-export default async function init(el) {
+export default function init(el) {
   const customElem = document.createElement('blade-changebg');
   customElem.classList.add('blade');
+
   // Extracting asset keys and urls and passing it to the custom element
   const images = el.querySelectorAll(':scope > div');
   images.forEach((item) => {
@@ -17,15 +26,18 @@ export default async function init(el) {
     const src = content.querySelectorAll(':scope > picture > img')[0]?.src || content.querySelectorAll(':scope > a')[0]?.href;
     customElem.setAttribute(keyName, src);
   });
-  el.innerHTML = '';
-  el.append(customElem);
+
+  const firstImage = images[1]?.children[1];
+  const src = firstImage.querySelectorAll(':scope > picture > img')[0]?.src || firstImage.querySelectorAll(':scope > a')[0]?.href;
+  const placeHolderImg = createTag('img', { src, class: 'blade' });
 
   // Render it inside marquee if its previous sibling is a marquee block
   if (el.previousElementSibling?.classList.contains('marquee')) {
     const marquee = el.previousElementSibling;
     const container = marquee.querySelectorAll(':scope > div:last-child > div:last-child')[0];
-    container.innerHTML = '';
-    container.append(customElem);
+    renderAsset(container, customElem, placeHolderImg);
     el.remove();
+  } else {
+    renderAsset(el, customElem, placeHolderImg);
   }
 }
