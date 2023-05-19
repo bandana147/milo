4c5493e58472a290cf9ac47e4e16766f5932e8f1
Laishram Bandana Devi <laishrambandanadevi@Laishrams-MacBook-Pro.local>
2023-05-19 16:35:08 +0530

    fix: optimization

--- libs/blocks/change-bg/change-bg.js
+++ libs/blocks/change-bg/change-bg.js
@@ -11,42 +11,35 @@ const assetKeys = {
 const config = getConfig();
 const base = config.miloLibs || config.codeRoot;
 
-function renderAsset(container, placeHolderImg, images) {
-  container.innerHTML = '';
-  container.append(placeHolderImg);
-
-  placeHolderImg.addEventListener('load', () => {
-    setTimeout(() => {
-      import(`${base}/deps/blades/blade-changebg.js`);
-      const customElem = document.createElement('blade-changebg');
-      customElem.classList.add('blade');
-      // Extracting asset keys and urls and passing it to the custom element
-      images.forEach((item) => {
-        const { children } = item;
-        const keyName = children[0].textContent;
-        const content = children[1];
-        const src = content.querySelectorAll(':scope > picture > img')[0]?.src || content.querySelectorAll(':scope > a')[0]?.href;
-        customElem.setAttribute([assetKeys[keyName]], src);
-      });
-      container.innerHTML = '';
-      container.append(customElem);
-    }, 3000);
+function renderBlade(container, images) {
+  import(`${base}/deps/blades/blade-changebg.js`);
+  const customElem = document.createElement('blade-changebg');
+  customElem.classList.add('blade');
+  // Extracting asset keys and urls and passing it to the custom element
+  images.forEach((item) => {
+    const { children } = item;
+    const keyName = children[0].textContent;
+    const content = children[1];
+    const src = content.querySelectorAll(':scope > picture > img')[0]?.src || content.querySelectorAll(':scope > a')[0]?.href;
+    customElem.setAttribute([assetKeys[keyName]], src);
   });
+  container.innerHTML = '';
+  container.append(customElem);
 }
 
 export default function init(el) {
   const images = el.querySelectorAll(':scope > div');
   const firstImage = images[0]?.children[1];
   const src = firstImage.querySelectorAll(':scope > picture > img')[0]?.src || firstImage.querySelectorAll(':scope > a')[0]?.href;
-  const placeHolderImg = createTag('img', { src, class: 'blade' });
-
   // Render it inside marquee if its previous sibling is a marquee block
   if (el.previousElementSibling?.classList.contains('marquee')) {
     const marquee = el.previousElementSibling;
     const container = marquee.querySelectorAll(':scope > div:last-child > div:last-child')[0];
-    renderAsset(container, placeHolderImg, images);
     el.remove();
+    setTimeout(() => {
+      renderBlade(container, images);
+    }, 3000);
   } else {
-    renderAsset(el, placeHolderImg, images);
+    renderBlade(el, images);
   }
 }
