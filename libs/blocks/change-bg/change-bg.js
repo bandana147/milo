c3d691a25442407b93911cb50461cefe2decbd8f
Laishram Bandana Devi <laishrambandanadevi@Laishrams-MacBook-Pro.local>
2023-05-29 16:59:36 +0530

    fix: schema design

--- libs/blocks/change-bg/change-bg.js
+++ libs/blocks/change-bg/change-bg.js
@@ -47,4 +47,37 @@ export default function init(el) {
   } else {
     renderAsset(el, placeHolderImg, images);
   }
+
+  // const labels = el.querySelectorAll(':scope > div:nth-child(odd)');
+  // const customElem = document.createElement('blade-changebg');
+  // customElem.classList.add('blade');
+  
+  // //construct the attribute here and pass
+  // const items = [...labels].map((label, idx) => {
+  //   const type = label.children[0].textContent;
+  //   const images = (label.nextElementSibling.firstElementChild).querySelectorAll(':scope > picture');
+  //   customElem.setAttribute(`type-${idx+1}`, type);
+  //   images.forEach((img, imgIndex)=> {
+  //     const src = img.getElementsByTagName('img')[0]?.src || img.getElementsByTagName(':scope > a')[0]?.href;
+  //     customElem.setAttribute(`${type}${imgIndex+1}Url`, src);
+  //   })
+  // });
+  // console.log(customElem)
+
+  // //pass the schema
+  // const schema = [];
+  // [...labels].map((label) => {
+  //   let item = {};
+  //   const type = label.children[0].textContent;
+  //   item.label = type;
+  //   item.images = [];
+  //   const images = (label.nextElementSibling.firstElementChild).querySelectorAll(':scope > picture');
+  //   images.forEach((img)=> {
+  //     const src = img.getElementsByTagName('img')[0]?.src || img.getElementsByTagName(':scope > a')[0]?.href;
+  //     item.images.push(src);
+  //   })
+  //   schema.push(item)
+  // });
+
+  // console.log(schema);
 }
