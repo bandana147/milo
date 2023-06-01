27e67e35606eedffa7e38b3e3881473a0f9bcdfe
Laishram Bandana Devi <laishrambandanadevi@Laishrams-MacBook-Pro.local>
2023-06-01 13:25:45 +0530

    fix: js

--- libs/blocks/version-history/version-history.js
+++ libs/blocks/version-history/version-history.js
@@ -1,6 +1,5 @@
 import { html, render } from '../../deps/htm-preact.js';
 
-export default async function init(el) {
-  const x = await fetch('https://version-history--milo--bandana147.hlx.page/drafts/blaishram/document1.plain.html');
-  render(x, el);
+export default function init(el) {
+  render(html`Hello world`, el);
 }
