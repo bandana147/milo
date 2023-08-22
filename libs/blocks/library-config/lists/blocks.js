765043357b4acbce0e8239020a73d37bc8eff89c
Bandana Laishram <laishrambandanadevi@Laishrams-MacBook-Pro.local>
2023-08-22 23:21:17 +0530

    Fix: remove unused code

--- libs/blocks/library-config/lists/blocks.js
+++ libs/blocks/library-config/lists/blocks.js
@@ -37,26 +37,20 @@ function getBlockName(block) {
   return classes.length > 0 ? `${name} (${classes.join(', ')})` : name;
 }
 
-function getTable(block, name, path) {
-  const url = new URL(path);
-  block.querySelectorAll('img').forEach((img) => {
-    const srcSplit = img.src.split('/');
-    const mediaPath = srcSplit.pop();
-    img.src = `${url.origin}/${mediaPath}`;
-    const { width, height } = img;
-    const ratio = width > 200 ? 200 / width : 1;
-    img.width = width * ratio;
-    img.height = height * ratio;
-    const pic = img.closest('picture');
-    pic.parentElement.replaceChild(img, pic);
-  });
+function getContainerName(container) {
+  const firstBlock = container.elements?.[0];
+  return getAuthorName(container) || getBlockName(firstBlock);
+}
+
+function getTable(block) {
+  const name = getBlockName(block);
   const rows = [...block.children];
   const maxCols = rows.reduce((cols, row) => (
     row.children.length > cols ? row.children.length : cols), 0);
   const table = document.createElement('table');
   table.setAttribute('border', 1);
   const headerRow = document.createElement('tr');
-  headerRow.append(createTag('td', { colspan: maxCols }, name));
+  headerRow.append(createTag('th', { colspan: maxCols }, name));
   table.append(headerRow);
   rows.forEach((row) => {
     const tr = document.createElement('tr');
