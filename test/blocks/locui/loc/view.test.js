fbe195271bde1a521594b731c8f9b1ac20f071c5
Bandana Laishram <laishrambandanadevi@laishrams-mbp.corp.adobe.com>
2023-08-29 14:47:21 +0530

    Test case wip

---
+++ test/blocks/locui/loc/view.test.js
@@ -0,0 +1,39 @@
+import { expect } from '@esm-bundle/chai';
+import sinon from 'sinon';
+import { html, render } from '../../../../libs/deps/htm-preact.js';
+import { waitForElement } from '../../../helpers/waitfor.js';
+import View from '../../../../libs/blocks/locui/loc/view.js';
+import { mockFetch } from '../../../helpers/generalHelpers.js';
+
+const ogFetch = window.fetch;
+
+describe('View', () => {
+
+  afterEach(() => {
+    // Do not build up any test state - reset window.fetch to it's original state
+    window.fetch = ogFetch;
+  });
+
+  beforeEach(async () => {
+    window.msal = {
+      PublicClientApplication: function t() {
+        return {
+          getAllAccounts: () => [{ username: 'test' }],
+          loginPopup: sinon.stub().resolves(),
+          acquireTokenSilent: sinon.stub().resolves({ accessToken: 'fake-access-token' }),
+        };
+      },
+    };
+    const review = html`<${View} />`;
+    render(review, document.body);
+  });
+
+  it('should display the correct title', async () => {
+    const payload = [{ id: 1, name: 'Test' }];
+    window.fetch = mockFetch({payload})
+    const container = await waitForElement('.locui');
+    const firstTitleElem = container.querySelector('h1')[0];
+    console.log(firstTitleElem, 'firstTitleElem')
+    expect(firstTitleElem).to.equal('Milo Localizationd');
+  });
+});
--- test/blocks/locui/locui.test.js
+++ test/blocks/locui/locui.test.js
@@ -1,53 +1,58 @@
 import { expect } from '@esm-bundle/chai';
-import { setConfig } from '../../../libs/utils/utils.js';
-import { getUrlDetails } from '../../../libs/blocks/locui/utils/utils.js';
-import { delay } from '../../helpers/waitfor.js';
+// import { setConfig } from '../../../libs/utils/utils.js';
+// import { getUrlDetails } from '../../../libs/blocks/locui/utils/utils.js';
+// import { delay } from '../../helpers/waitfor.js';
+import { waitForElement } from '../../helpers/waitfor.js';
+// import init from '../../../libs/blocks/locui/locui.js';
 
-const config = {
-  codeRoot: '/libs',
-  locales: {
-    de: { ietf: 'de-de' },
-    '': { ietf: 'en-US' },
-  },
-};
-setConfig(config);
+// const config = {
+//   codeRoot: '/libs',
+//   locales: {
+//     de: { ietf: 'de-de' },
+//     '': { ietf: 'en-US' },
+//   },
+// };
+// setConfig(config);
 
-describe('Format URLs', () => {
-  it('Gets pathname', async () => {
-    const urls = [new URL('https://main--milo--adobecom.hlx.page/drafts/cmillar/test')];
-    const urlDetails = await getUrlDetails(urls);
-    expect(urlDetails[0].langstore.pathname).to.equal('/langstore/en/drafts/cmillar/test');
-  });
-});
+// describe('Format URLs', () => {
+//   it('Gets pathname', async () => {
+//     const urls = [new URL('https://main--milo--adobecom.hlx.page/drafts/cmillar/test')];
+//     const urlDetails = await getUrlDetails(urls);
+//     expect(urlDetails[0].langstore.pathname).to.equal('/langstore/en/drafts/cmillar/test');
+//   });
+// });
 
-describe('Format URLs', () => {
-  it('Gets pathname', async () => {
-    const urls = [new URL('https://main--milo--adobecom.hlx.page/de/drafts/cmillar/test')];
-    const urlDetails = await getUrlDetails(urls);
-    expect(urlDetails[0].langstore.pathname).to.equal('/langstore/de/drafts/cmillar/test');
-  });
-});
+// const configContentRoot = {
+//   codeRoot: '/libs',
+//   contentRoot: '/acrobat',
+//   locales: {
+//     de: { ietf: 'de-de' },
+//     '': { ietf: 'en-US' },
+//   },
+// };
+// setConfig(configContentRoot);
+// describe('Format URLs', () => {
+//   it('Gets pathname', async () => {
+//     const urls = [new URL('https://main--milo--adobecom.hlx.page/acrobat/drafts/cmillar/test')];
+//     const urlDetails = await getUrlDetails(urls);
+//     expect(urlDetails[0].langstore.pathname).to.equal('/langstore/en/acrobat/drafts/cmillar/test');
+//   });
+// });
+// describe('Format URLs', () => {
+//   it('Gets pathname', async () => {
+//     const urls = [new URL('https://main--milo--adobecom.hlx.page/de/acrobat/drafts/cmillar/test')];
+//     const urlDetails = await getUrlDetails(urls);
+//     expect(urlDetails[0].langstore.pathname).to.equal('/langstore/de/acrobat/drafts/cmillar/test');
+//   });
+// });
 
-const configContentRoot = {
-  codeRoot: '/libs',
-  contentRoot: '/acrobat',
-  locales: {
-    de: { ietf: 'de-de' },
-    '': { ietf: 'en-US' },
-  },
-};
-setConfig(configContentRoot);
-describe('Format URLs', () => {
-  it('Gets pathname', async () => {
-    const urls = [new URL('https://main--milo--adobecom.hlx.page/acrobat/drafts/cmillar/test')];
-    const urlDetails = await getUrlDetails(urls);
-    expect(urlDetails[0].langstore.pathname).to.equal('/langstore/en/acrobat/drafts/cmillar/test');
-  });
-});
-describe('Format URLs', () => {
-  it('Gets pathname', async () => {
-    const urls = [new URL('https://main--milo--adobecom.hlx.page/de/acrobat/drafts/cmillar/test')];
-    const urlDetails = await getUrlDetails(urls);
-    expect(urlDetails[0].langstore.pathname).to.equal('/langstore/de/acrobat/drafts/cmillar/test');
+
+describe('Version history Comp', () => {
+  it('could be initialized', async () => {
+    // document.body.innerHTML = '<div class="section"><div></div></div>';
+    // const div = document.querySelector('.section');
+    // await init(div);
+    // const review = await waitForElement('.locui');
+    expect(true).to.be.true;
   });
 });
