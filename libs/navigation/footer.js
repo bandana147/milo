e41077b24e2b94506f371bcdb7584d5a4b5ae2b3
Bandana Laishram <bandanalaishram@gmail.com>
2024-07-15 16:18:34 +0530

    Updating default branch to main

--- libs/navigation/footer.js
+++ libs/navigation/footer.js
@@ -4,22 +4,21 @@ const blockConfig = {
   appendType: 'appendChild',
 };
 
-async function loadBlock() {
-  const { fedsGlobalConfig = {} } = window;
-  const miloLibs = fedsGlobalConfig.miloLibs || 'https://stage--milo--adobecom.hlx.page';
+export default async function loadBlock(configs) {
+  const { locale,  contentRoot} = configs;
+  const miloLibs = configs.miloLibs || 'https://www.adobe.com';
 
   // Relative path can't be used, as the script will run on consumer's app
   const { default: bootstrapBlock } = await import(`${miloLibs}/libs/navigation/bootstrapper.js`);
   const { default: locales } = await import(`${miloLibs}/libs/utils/locales.js`);
   const clientConfig = {
+    contentRoot,
     origin: miloLibs,
-    contentRoot: fedsGlobalConfig.contentRoot,
     miloLibs: `${miloLibs}/libs`,
-    pathname: `/${fedsGlobalConfig.locale || ''}`,
-    locales: fedsGlobalConfig.locales || locales,
+    pathname: `/${locale || ''}`,
+    locales: configs.locales || locales,
   };
 
   bootstrapBlock(clientConfig, blockConfig);
 }
 
-loadBlock();
