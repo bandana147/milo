98d2bd4c521adafc860e0efead7ccf8534b2ebd5
Bandana Laishram <bandanalaishram@gmail.com>
2024-07-24 18:32:16 +0530

    Adding global nav WIP

--- libs/navigation/index.js
+++ libs/navigation/index.js
@@ -6,6 +6,6 @@ const locale = new URLSearchParams(window.location.search).get('locale');
 async function init() {
   const scriptEnv = env === 'qa' ? 'feds' : env;
   const { default: loadBlock } = await import(`https://${scriptEnv}--milo--adobecom.hlx.page/libs/navigation/navigation.js`);
-  loadBlock({ env, locale, footer: { authoringPath, privacyId }});
+  loadBlock({ env, locale, footer: { authoringPath, privacyId }, header: { authoringPath, imsClientId: 'milo' } });
 }
 init();
