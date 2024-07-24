4132c9616f167c1104e03620eec623dda6a4eaab
Bandana Laishram <bandanalaishram@gmail.com>
2024-07-24 18:39:05 +0530

    Adding css

--- libs/navigation/index.js
+++ libs/navigation/index.js
@@ -4,8 +4,7 @@ const privacyId = new URLSearchParams(window.location.search).get('privacyid');
 const locale = new URLSearchParams(window.location.search).get('locale');
 
 async function init() {
-  const scriptEnv = env === 'qa' ? 'feds' : env;
-  const { default: loadBlock } = await import(`https://${scriptEnv}--milo--adobecom.hlx.page/libs/navigation/navigation.js`);
+  const { default: loadBlock } = await import(`https://${env}--milo--adobecom.hlx.page/libs/navigation/navigation.js`);
   loadBlock({ env, locale, footer: { authoringPath, privacyId }, header: { authoringPath, imsClientId: 'milo' } });
 }
 init();
