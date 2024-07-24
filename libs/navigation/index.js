04b2045b6ac0913e113b5fba7b5a4f3d62aad822
Bandana Laishram <bandanalaishram@gmail.com>
2024-07-24 18:12:15 +0530

    Making script env configurable

---
+++ libs/navigation/index.js
@@ -0,0 +1,11 @@
+const authoringPath = new URLSearchParams(window.location.search).get('authoringpath') || '/federal/home';
+const env = new URLSearchParams(window.location.search).get('env') || 'qa';
+const privacyId = new URLSearchParams(window.location.search).get('privacyid');
+const locale = new URLSearchParams(window.location.search).get('locale');
+
+async function init() {
+  const scriptEnv = env === 'qa' ? 'feds' : env;
+  const { default: loadBlock } = await import(`https://${scriptEnv}--milo--adobecom.hlx.page/libs/navigation/navigation.js`);
+  loadBlock({ env, locale, footer: { authoringPath, privacyId }});
+}
+init();
