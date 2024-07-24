ca7b61e932535c58f4e609b64409b25605e5591d
Bandana Laishram <bandanalaishram@gmail.com>
2024-07-24 18:54:29 +0530

    Adding css

--- libs/navigation/index.js
+++ libs/navigation/index.js
@@ -2,9 +2,10 @@ const authoringPath = new URLSearchParams(window.location.search).get('authoring
 const env = new URLSearchParams(window.location.search).get('env') || 'qa';
 const privacyId = new URLSearchParams(window.location.search).get('privacyid');
 const locale = new URLSearchParams(window.location.search).get('locale');
+const navBranch = new URLSearchParams(window.location.search).get('navbranch');
 
 async function init() {
-  const { default: loadBlock } = await import(`https://${env}--milo--adobecom.hlx.page/libs/navigation/navigation.js`);
+  const { default: loadBlock } = await import(`https://${navBranch}--milo--adobecom.hlx.page/libs/navigation/navigation.js`);
   loadBlock({ env, locale, footer: { authoringPath, privacyId }, header: { authoringPath, imsClientId: 'milo' } });
 }
 init();
